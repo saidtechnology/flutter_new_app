f2ff429cde5e756d1240b6022ba92bc73b646286
saidtechnology <saidx5@gmail.com>
2019-09-24 04:55:33 +0100

    add dispose of Tab Controller Headline News Screen to Protect Memory Size

--- lib/screens/headline_news.dart
+++ lib/screens/headline_news.dart
@@ -17,6 +17,12 @@ class _HeadLineNewsState extends State<HeadLineNews> with SingleTickerProviderSt
     super.initState();
     _tabController = TabController(initialIndex: 0, length: 3, vsync: this);
   }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
